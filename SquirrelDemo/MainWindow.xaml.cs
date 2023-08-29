f649c046c75c8cc0547027e0aef40cc32b27c6e0
wmunoz <wmunoz@synergiescorp.com>
2023-08-29 13:59:09 -0400

    Added update code

--- SquirrelDemo/MainWindow.xaml.cs
+++ SquirrelDemo/MainWindow.xaml.cs
@@ -1,15 +1,41 @@
-﻿using System.Windows;
+﻿using Squirrel;
+using System;
+using System.Windows;
 
-namespace SquirrelDemo
+namespace SquirrelDemo;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    UpdateManager? updateManager;
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+        Loaded += MainWindow_Load;
+    }
+
+    private async void MainWindow_Load(object sender, RoutedEventArgs e)
+    {
+        updateManager = await UpdateManager.GitHubUpdateManager("https://github.com/wellington0718/SquirrelDemo");
+        CurrentVersionTextBox.Text = updateManager.CurrentlyInstalledVersion().ToString();
+    }
+
+    private async void CheckForUpdates_Click(object sender, RoutedEventArgs e)
+    {
+        var updateAvailable = await updateManager!.CheckForUpdate();
+
+        if (updateAvailable.ReleasesToApply.Count > 0)
         {
-            InitializeComponent();
+            UpdateButton.IsEnabled = true;
         }
+        else
+        {
+            UpdateButton.IsEnabled = false;
+        }
+    }
+
+    private async void Update_Click(object sender, RoutedEventArgs e)
+    {
+       await updateManager!.UpdateApp();
+        MessageBox.Show("Updated successfuly");
     }
 }
